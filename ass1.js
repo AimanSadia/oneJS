7c44835bf1d0095c5c94229a7e2bc1e89bf3ed7f
Aiman Sdia <102213880+AimanSadia@users.noreply.github.com>
2023-05-16 16:33:57 +0500

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,32 @@
+/* -----------------------ASSIGNMENT 1 -------------------------------- */
+
+/* ===========QUESTION NO 01 =============*/
+
+/* alert('Hello Aiman, welcome to our web page'); */
+
+/* ===========QUESTION NO 02 =============*/
+
+/* alert('Error, please enter a valid password'); */
+
+/* ===========QUESTION NO 03 =============*/
+
+/* var message = 'Welcome to JS land....\n';
+message += 'Happy Coding';
+alert(message); */
+
+/* ===========QUESTION NO 04 =============*/
+
+/* alert('Welcome to JS land...');
+alert('Happy Coding'); */
+
+/* ===========QUESTION NO 05 =============*/
+
+/* alert('Hello, I can run javascript through my web browser console'); */
+
+/* ===========QUESTION NO 06 =============*/
+
+/* alert('Welcome to our website!!!'); */
+
+/* ===========QUESTION NO 07 =============*/
+
+/* practice is done numerous times inside html file */
